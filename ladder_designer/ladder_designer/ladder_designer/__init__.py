08db5a3503fc45b6638cebe15f7a28781e38556d
Яна Володина <shanna.666@mail.ru>
2018-10-21 15:04:24 +1000

    initial commit project

---
+++ ladder_designer/ladder_designer/ladder_designer/__init__.py
@@ -0,0 +1,3 @@
+"""
+Package for ladder_designer.
+"""
---
+++ ladder_designer/ladder_designer/ladder_designer/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for ladder_designer.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', ladder_designer.views.home, name='home'),
+    # url(r'^ladder_designer/', include('ladder_designer.ladder_designer.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
