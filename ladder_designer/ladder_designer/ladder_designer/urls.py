ca15fe10608944495c739f89d26046f964057f53
Яна Володина <shanna.666@mail.ru>
2018-10-22 13:32:38 +1000

    created 4 blocks

--- ladder_designer/ladder_designer/ladder_designer/urls.py
+++ ladder_designer/ladder_designer/ladder_designer/urls.py
@@ -3,14 +3,14 @@ Definition of urls for ladder_designer.
 """
 
 from django.conf.urls import include, url
-
+import ladder_designer_app.views
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', ladder_designer.views.home, name='home'),
+    url(r'^$', ladder_designer_app.views.index, name='index')
     # url(r'^ladder_designer/', include('ladder_designer.ladder_designer.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
